e2e0a27567d3d34dfbb7340db59eca19e9e51ee8
Ravi Subramanian <ravi.subram@gmail.com>
2017-11-06 13:46:16 -0600

    step 9 success

--- todolists/config/routes.rb
+++ todolists/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 
   resources :todo_lists do
     resources :todo_items
